df455e62c7a45007d69ad7115e4c57bf9746f08e
Max <pogatop@yandex.ru>
2022-12-22 22:24:23 +0300

    Init

---
+++ SickLeaveTelegramBot.App/Program.cs
@@ -0,0 +1,10 @@
+using SickLeaveTelegramBot.App;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
